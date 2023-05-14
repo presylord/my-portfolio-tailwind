338886f5a05edcea76636e80bd4936a53f703f99
Presy Lord Kidlat <kidlat123@gmail.com>
2023-05-14 15:04:04 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from "./components/About"
+import Home from "./components/Home"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react"
+import Logo from "../assets/logo1.png"
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa"
+import { HiOutlineMail } from "react-icons/hi"
+import { BsFillPersonLinesFill } from "react-icons/bs"
+
+const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false)
+    const handleClick = () => {
+        setToggle(!toggle)
+
+    }
+
+    return (
+        <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#08192f] text-gray-300">
+            <div>
+                <img src={Logo} style={{ width: '50px' }} />
+            </div>
+            {/* Menu */}
+            <ul className="hidden md:flex">
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+
+            {/* Hamburger */}
+            <div className="md:hidden z-10" onClick={handleClick} >{toggle ? <FaTimes /> : <FaBars />}</div>
+
+            {/* Mobile Menu */}
+            <ul className={toggle ? "absolute top-0 left-0 w-full h-screen bg-[#08192f] flex flex-col justify-center items-center" : "hidden"} >
+                <li className="py-6 text-4xl">Home</li>
+                <li className="py-6 text-4xl">About</li>
+                <li className="py-6 text-4xl">Skills</li>
+                <li className="py-6 text-4xl">Projects</li>
+                <li className="py-6 text-4xl">Contact</li>
+            </ul>
+
+            {/* Social Icons */}
+            <div className="hidden md:flex fixed flex-col top-[35%] left-0">
+                <ul>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+                        <a className="flex justify-between items-center w-full" href="#">LinkedIn<FaLinkedin size={30} /></a>
+                    </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+                        <a className="flex justify-between items-center w-full" href="#">Github<FaGithub size={30} /></a>
+                    </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
+                        <a className="flex justify-between items-center w-full" href="#">Email<HiOutlineMail size={30} /></a>
+                    </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
+                        <a className="flex justify-between items-center w-full" href="#">Resume<BsFillPersonLinesFill size={30} /></a>
+                    </li>
+                </ul>
+            </div >
+
+        </div >
+    )
+}
+export default Navbar
