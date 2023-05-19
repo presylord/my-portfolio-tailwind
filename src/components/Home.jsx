a5053e92fe386d566d4c4d35e9b4a5a83191250c
presy-jbbgi <kidlat123@gmail.com>
2023-05-19 11:07:20 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,15 +30,14 @@ const Home = () => {
         </h2>
 
         <div className="pt-4">
-          <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#19A7CE] hover:border-[#19A7CE]">
-            <Link to="projects" smooth={true} duration={500}>
+          <Link to="projects" smooth={true} duration={500}>
+            <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#19A7CE] hover:border-[#19A7CE]">
               View Projects
-            </Link>
-
-            <span className="group-hover:rotate-90 duration-300">
-              <HiArrowNarrowRight className="ml-3" />
-            </span>
-          </button>
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
