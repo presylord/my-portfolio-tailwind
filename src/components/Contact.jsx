d7eeb9a99b602754ba746410e9e2476debe77ba0
Presy Lord Kidlat <kidlat123@gmail.com>
2023-06-11 15:17:37 +0800

    added notifications when sending a message

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,16 @@
 import { useRef } from "react";
 import emailjs from "@emailjs/browser";
+import toast, { Toaster } from 'react-hot-toast';
+
 
 const Contact = () => {
+
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault(); // prevents the page from reloading when you hit “Send”
-    console.log(form.current);
+    // console.log(form.current);
+    toast.loading('Sending message...');
+
     emailjs
       .sendForm(
         "service_qzmb9o4",
@@ -15,12 +20,20 @@ const Contact = () => {
       )
       .then(
         (result) => {
-          // show the user a success message
-          console.log("sent", result);
+          // console.log("sent", result);
+          form.current.reset();
+          toast.dismiss()
+          toast.success('Message successfully sent!', { duration: 6000 }
+          )
+
         },
         (error) => {
           // show the user an error
-          console.log(error);
+          // console.log(error);
+          form.current.reset();
+          toast.dismiss()
+          toast.error('Encountered an error.', { duration: 6000 })
+
         }
       );
   };
@@ -48,23 +61,28 @@ const Contact = () => {
           className="  p-2 rounded-lg	"
           placeholder="Name"
           name="from_name"
+          required
         />
         <input
           type="email"
           className="my-4 p-2  rounded-lg	"
           placeholder="Email"
           name="reply_to"
+          required
         />
         <textarea
           className=" p-2 rounded-lg	"
           placeholder="Message"
           name="message"
           rows="10"
+          required
         />
         <button className="text-white border-2 hover:bg-[#19A7CE] hover:border-[#19A7CE] px-4 py-3 my-8 mx-auto flex items-center">
           Contact Me
         </button>
       </form>
+      <Toaster position="bottom-center" />
+
     </div>
   );
 };
